7f1d84d1722c30a3841caf75bc858b080cc8552e
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2020-12-31 09:41:26 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace bachelor_party
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int targetMoney = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            int totalPrice = 0;
+            int totalPerson = 0;
+
+
+            while (command != "The restaurant is full")
+            {
+                int groupSize = int.Parse(command);
+                totalPerson += groupSize;
+                int pricePerPerson = 0;
+                command = Console.ReadLine();
+
+                if (groupSize < 5)
+                {
+
+                    pricePerPerson = 100;
+                }
+
+                else
+                {
+                    pricePerPerson = 70;
+                }
+
+                int totalGroupPrice = groupSize * pricePerPerson;
+
+                totalPrice += totalGroupPrice;
+            }
+
+            if (totalPrice>=targetMoney)
+            {
+                Console.WriteLine($"You have {totalPerson} guests and {totalPrice - targetMoney} leva left.");
+            }
+
+            else
+            {
+                Console.WriteLine($"You have {totalPerson} guests and {totalPrice} leva income, but no singer.");
+            }
+
+
+            
+        }
+    }
+}
